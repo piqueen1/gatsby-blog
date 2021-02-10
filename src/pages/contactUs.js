719349dc36d591b540b2cb45a561f8fea8b65e90
Finn Thye <piqueen@gmail.com>
2021-02-09 18:30:58 -0700

    Add recaptcha library

--- src/pages/contactUs.js
+++ src/pages/contactUs.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import ReCAPTCHA from 'react-google-recaptcha'
 //import Layout from '../components/layout'
+
 
 const ContactFormPage = () => (
   //  <Layout>
       <div>
         <h1>Contact</h1>
+
+        <form 
+            name="Contact Form" 
+            method="POST" 
+            data-netlify="true"
+            action="/thank-you"
+            data-netlify-recaptcha="true"
+        >
+            <input type="hidden" name="form-name" value="Contact Form" />
+            <div>
+                <label>Your Email:</label>
+                <input type="email" name="email" />
+            </div>
+            <div>
+                <label>Message:</label>
+                <textarea name="message" />
+            </div>
+            <br />
+            <ReCAPTCHA sitekey="6LedP1AaAAAAALpGop-3R2KlfgwL69ypbTSugoES" />
+            <button type="submit">Send</button>
+        </form>
       </div>
   //  </Layout>
 )
---
+++ src/pages/thank-you.js
@@ -0,0 +1,13 @@
+import React from "react"
+//import Layout from "../components/layout"
+
+const ThankYouPage = () => (
+  //<Layout>
+  <div>
+    <h1>Contact</h1>
+    <p>Thank you for your submission!</p>
+  </div>
+  //</Layout>
+)
+
+export default ThankYouPage
